aa9cdbd50396c22b870b3787e6e484e68d235d60
Evandro <evandro.ignacio@fcamara.com.br>
2020-01-16 13:48:08 -0300

    Foi criada a tela inicial com o formulário de cadastro

---
+++ web/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import './Global.css';
+import './App.css'
+import './Sidebar.css'
+
+function App() {
+  return <div id="app">
+    <aside>
+      <strong>Cadastrar</strong>
+      <form>
+        <div class="input-block">
+          <label htmlFor="">Usuário do Github</label>
+          <input name="github_username" id="github_username" required/>
+        </div>
+
+        <div class="input-block">
+          <label htmlFor="">Tecnologias</label>
+          <input name="github_username" id="github_username" required/>
+        </div>
+
+        <div className="input-group">
+          <div class="input-block">
+            <label htmlFor="">Latitude</label>
+            <input name="latitude" id="latitude" required/>
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="">Longitude</label>
+            <input name="longitude" id="longitude" required/>
+          </div>
+        </div>
+
+        <button type="submit">Salvar</button>
+      </form>
+    </aside>
+    <main>
+    </main>
+  </div>
+}
+
+export default App;
