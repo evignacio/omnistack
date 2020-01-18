2ef8176d1d31aa1a725958279e76ac91c0bd5ced
Evandro <evandro.ignacio@fcamara.com.br>
2020-01-18 17:04:42 -0300

    Foi feito os cards dos usuários

--- web/src/App.js
+++ web/src/App.js
@@ -1,40 +1,128 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './service/api';
 
 import './Global.css';
 import './App.css'
 import './Sidebar.css'
+import './Main.css';
 
 function App() {
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [github_username, setGithubUserName] = useState('');
+  const [techs, setTechs] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+       const { latitude, longitude} = position.coords;
+
+       setLongitude(longitude);
+       setLatitude(latitude);
+      },
+      (error) => {
+        console.log(error.message);
+      }
+    )
+  })
+
+  async function handleAddDev(event) {
+    event.preventDefault();
+    
+    try {
+      await api.post('/dev', {
+        github_username,
+        techs,
+        latitude,
+        longitude
+      });
+      alert('usuário salvo com sucesso');  
+    } catch(error) {
+      alert(error);
+    }
+  }
+
   return <div id="app">
     <aside>
       <strong>Cadastrar</strong>
       <form>
-        <div class="input-block">
+        <div className="input-block">
           <label htmlFor="">Usuário do Github</label>
-          <input name="github_username" id="github_username" required/>
+          <input name="github_username" id="github_username" required value={github_username} onChange={e => setGithubUserName(e.target.value)}/>
         </div>
 
-        <div class="input-block">
+        <div className="input-block">
           <label htmlFor="">Tecnologias</label>
-          <input name="github_username" id="github_username" required/>
+          <input name="techs" id="techs" required value={techs} onChange={e => setTechs(e.target.value)}/>
         </div>
 
         <div className="input-group">
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="">Latitude</label>
-            <input name="latitude" id="latitude" required/>
+            <input name="latitude" id="latitude" required type="number" value={latitude} onChange={e => setLatitude(e.target.value)}/>
           </div>
 
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="">Longitude</label>
-            <input name="longitude" id="longitude" required/>
+            <input name="longitude" id="longitude" required type="number" value={longitude} onChange={e => setLongitude(e.target.value)} />
           </div>
         </div>
 
-        <button type="submit">Salvar</button>
+        <button type="submit" onClick={e => handleAddDev(e)} >Salvar</button>
       </form>
     </aside>
     <main>
+      <ul>
+
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars2.githubusercontent.com/u/39386199?s=460&v=4"/>
+            <div className="user-info">
+              <strong>Evandro Ignácio</strong>
+              <span>html, javaScript, Java</span>
+            </div>
+          </header>
+          <p>O Fabuloso Gerador de Lero-lero v2.0 é capaz de gerar qualquer</p>
+          <a href="https://github.com/evignacio">Acessa perfil no GitHub</a>
+        </li>
+        
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars2.githubusercontent.com/u/39386199?s=460&v=4"/>
+            <div className="user-info">
+              <strong>Evandro Ignácio</strong>
+              <span>html, javaScript, Java</span>
+            </div>
+          </header>
+          <p>O Fabuloso Gerador de Lero-lero v2.0 é capaz de gerar qualquer</p>
+          <a href="https://github.com/evignacio">Acessa perfil no GitHub</a>
+        </li>
+
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars2.githubusercontent.com/u/39386199?s=460&v=4"/>
+            <div className="user-info">
+              <strong>Evandro Ignácio</strong>
+              <span>html, javaScript, Java</span>
+            </div>
+          </header>
+          <p>O Fabuloso Gerador de Lero-lero v2.0 é capaz de gerar qualquer</p>
+          <a href="https://github.com/evignacio">Acessa perfil no GitHub</a>
+        </li>
+
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars2.githubusercontent.com/u/39386199?s=460&v=4"/>
+            <div className="user-info">
+              <strong>Evandro Ignácio</strong>
+              <span>html, javaScript, Java</span>
+            </div>
+          </header>
+          <p>O Fabuloso Gerador de Lero-lero v2.0 é capaz de gerar qualquer</p>
+          <a href="https://github.com/evignacio">Acessa perfil no GitHub</a>
+        </li>
+
+      </ul>
     </main>
   </div>
 }
